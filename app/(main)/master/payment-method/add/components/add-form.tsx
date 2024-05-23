7168c72bc57effe59e53b89bc5490500c2ecab31
Iqbal <mhdiqbaltan@gmail.com>
2024-05-23 16:23:47 +0700

    add next progress bar

--- app/(main)/master/payment-method/add/components/add-form.tsx
+++ app/(main)/master/payment-method/add/components/add-form.tsx
@@ -13,7 +13,7 @@ import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next-nprogress-bar";
 import { PaymentMethdoSchema } from "@/src/entity/payment-method-entity";
 import { paymentMethodService } from "@/src/service/payment-method";
 
